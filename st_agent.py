830873eada721cb97e9a54ca42a533934571ce58
Simon Liu <xpliu203@gmail.com>
2023-08-27 22:17:13 +0800

    Integration with streamlit.

--- st_agent.py
+++ st_agent.py
@@ -29,7 +29,7 @@ def query_agent(agent, query):
     """
     Query an agent and return response with specified schema for streamlit to demo.
     """
-    prompt = (
+    prompt1 = (
     """
         For the following query, if it requires drawing a table, reply as follows:
         {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}}
@@ -63,8 +63,43 @@ def query_agent(agent, query):
         """
     + query
     )
+
+    prompt2 = (
+        """
+            Let's decode the way to respond to the queries. The responses depend on the type of information requested in the query. 
+
+            1. If the query requires a table, format your answer like this:
+            {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}}
+
+            2. For a bar chart, respond like this:
+            {"bar": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
+
+            3. If a line chart is more appropriate, your reply should look like this:
+            {"line": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
+
+            Note: We only accommodate two types of charts: "bar" and "line".
+
+            4. For a plain question that doesn't need a chart or table, your response should be:
+            {"answer": "Your answer goes here"}
+
+            For example:
+            {"answer": "The Product with the highest Orders is '15143Exfo'"}
+
+            5. If the answer is not known or available, respond with:
+            {"answer": "I do not know."}
+
+            Return all output as a string. Remember to encase all strings in the "columns" list and data list in double quotes. 
+            For example: {"columns": ["Products", "Orders"], "data": [["51993Masc", 191], ["49631Foun", 152]]}
+
+            Now, let's tackle the query step by step. Here's the query for you to work on: 
+            """
+        + query
+    )
     
-    response = agent.run(prompt)
+    print('promts:')
+    print(prompt2)
+
+    response = agent.run(prompt2)
     return response.__str__()
 
 
--- st_ui.py
+++ st_ui.py
@@ -14,8 +14,12 @@ def decode_response(response: str) -> dict:
     Returns:
         dict: dictionary with response data
     """
-    target = "{" + response.split("{")[1].split("}")[0] + "}"
-    print('Debug:', target)
+    print('Debug: original response', response)
+    print('Debug: end of original response')
+    target = response.split("Question")[0].strip().replace("'", '"')
+    if target and target.endswith("<|im_end|>"):
+        target = target[:-len("<|im_end|>")]
+    print('Debug: target response', target)
     print('Debug finished.####')
     return json.loads(target)
 
@@ -38,6 +42,65 @@ def write_response(response_dict: dict):
         df = pd.DataFrame(data)
         df.set_index("columns", inplace=True)
         st.line_chart(df)
+
+    # Check if the response is a table.
+    if "table" in response_dict:
+        data = response_dict["table"]
+        df = pd.DataFrame(data["data"], columns=data["columns"])
+        st.table(df)
+
+def decode_response2(response: str) -> dict:
+    """This function converts the string response from the model to a dictionary object.
+
+    Args:
+        response (str): response from the model
+
+    Returns:
+        dict: dictionary with response data
+    """
+    return json.loads(response)
+
+def write_answer2(response_dict: dict):
+    """
+    Write a response from an agent to a Streamlit app.
+
+    Args:
+        response_dict: The response from the agent.
+
+    Returns:
+        None.
+    """
+
+    # Check if the response is an answer.
+    if "answer" in response_dict:
+        st.write(response_dict["answer"])
+
+    # Check if the response is a bar chart.
+    # Check if the response is a bar chart.
+    if "bar" in response_dict:
+        data = response_dict["bar"]
+        try:
+            df_data = {
+                    col: [x[i] if isinstance(x, list) else x for x in data['data']]
+                    for i, col in enumerate(data['columns'])
+                }       
+            df = pd.DataFrame(df_data)
+            df.set_index("Products", inplace=True)
+            st.bar_chart(df)
+        except ValueError:
+            print(f"Couldn't create DataFrame from data: {data}")
+
+# Check if the response is a line chart.
+    if "line" in response_dict:
+        data = response_dict["line"]
+        try:
+            df_data = {col: [x[i] for x in data['data']] for i, col in enumerate(data['columns'])}
+            df = pd.DataFrame(df_data)
+            df.set_index("Products", inplace=True)
+            st.line_chart(df)
+        except ValueError:
+            print(f"Couldn't create DataFrame from data: {data}")
+
 
     # Check if the response is a table.
     if "table" in response_dict:
